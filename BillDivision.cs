8d637c3257b33687dd052c8db9f42448e3df7470
Abdur Rehman <71170538+Rusty002@users.noreply.github.com>
2024-05-17 07:34:29 +0500

    Create BillDivision.cs

---
+++ BillDivision.cs
@@ -0,0 +1,58 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'bonAppetit' function below.
+     *
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY bill
+     *  2. INTEGER k
+     *  3. INTEGER b
+     */
+
+    public static void bonAppetit(List<int> bill, int k, int b)
+    {
+        var amountToBeReturned = 0;
+        var sum = 0;
+        for(int i = 0; i < bill.Count; i++)
+            if(i != k)
+                sum += bill[i];
+                
+        sum /= 2; 
+        amountToBeReturned = Math.Abs(sum - b);
+        Console.WriteLine(amountToBeReturned == 0 ? "Bon Appetit" : amountToBeReturned);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int k = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<int> bill = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(billTemp => Convert.ToInt32(billTemp)).ToList();
+
+        int b = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.bonAppetit(bill, k, b);
+    }
+}
